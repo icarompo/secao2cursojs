a90345eae2c4edd2acb1b08840f00c66ac30346b
icarompo <icaromesquitaponce@outlook.com>
2023-04-26 16:53:33 -0300

    salvando primeiros feitos

---
+++ aula13/main.js
@@ -0,0 +1,7 @@
+let nome = 'Icaro';
+let cidade = 'Palmas';
+let ano_nasc = 2003;
+let idade = 2023 - ano_nasc
+let texto = (nome + ' nasceu em 2003, tem ' + idade + ' anos de idade e mora em ' + cidade);
+
+console.log(texto);
