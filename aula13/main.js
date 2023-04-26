d783650f720d5609b5675a048407a761d7afc3c7
icarompo <icaromesquitaponce@outlook.com>
2023-04-26 16:59:13 -0300

    Alterando denovo

--- aula13/main.js
+++ aula13/main.js
@@ -5,3 +5,5 @@ let idade = 2023 - ano_nasc
 let texto = (nome + ' nasceu em 2003, tem ' + idade + ' anos de idade e mora em ' + cidade);
 
 console.log(texto);
+console.log('Hello World');
+console.warn('OI');
