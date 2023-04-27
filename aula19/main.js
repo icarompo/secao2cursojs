3120b6eec4dbe7b068c2ffdcb1be257b4bc2ddca
icarompo <icaromesquitaponce@outlook.com>
2023-04-27 09:29:29 -0300

    Adicionando aula19

---
+++ aula19/main.js
@@ -0,0 +1,15 @@
+const nome = 'Icaro';//string
+const idade = 20;//number
+let nomeAluno;//undefined
+let sobrenomeAluno = null;//null
+let aprovado = false;//boolean
+
+let a = [1, 2];
+let b = a;
+
+console.log(a, b);
+
+b.push(3);
+
+console.log(a, b);
+console.log(typeof aprovado)
