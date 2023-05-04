c4630bd5cf148fe6e5e34645a691b6a3b0053b76
icarompo <icaromesquitaponce@outlook.com>
2023-05-04 10:27:19 -0300

    Adicionando aula27

---
+++ aula27/index.js
@@ -0,0 +1,23 @@
+let num = 10.510;
+
+//round the specified number to the next integer number
+console.log(Math.ceil(num));
+//round the specified number to the previous integer number
+console.log(Math.floor(num));
+//round the specified number to the nearest integer number
+console.log(Math.round(num));
+
+//return the largest number
+console.log(Math.max(1, 2, 3, 4, 5, 6));
+//return the smallest number
+console.log(Math.min(1, 2, 3, 4, 5, 6));
+
+//return a random number between 1 and 6
+console.log(Math.round(Math.random() * (1 - 6) + 6));
+
+//return PI value
+console.log(Math.PI);
+
+//return an exponentiation
+console.log(Math.pow(8, 2));
+console.log(8 ** 2);
